fbbcf3385c50be3933d0a51d8353e33b88fd47cc
Craig2k <craigh0102@gmail.com>
2024-11-29 15:00:18 +0000

    huhu

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
 
+    private void Start() {
+        GameManager.instance.onPlay.AddListener(ActivatePlayer);
+    }
+
+    private void ActivatePlayer() {
+        gameObject.SetActive(true);
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.transform.tag == "obstacle") {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
             GameManager.instance.GameOver();
         }
     }
--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -5,11 +5,21 @@ using TMPro;
 
 public class UImanager : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI scoreUI;
+    [SerializeField] private GameObject startMenuUI;
 
     GameManager gm;
 
     private void Start() {
         gm = GameManager.instance;
+    }
+
+    public void PlayButtonHandler () {
+        gm.StartGame();
+
+        if (!startMenuUI)
+            Debug.Log("No Start Menu Assigned.");
+
+        startMenuUI.SetActive(false);
     }
 
     private void OnGUI() {
